ba2b5f2c7c4a40e396a725bc29d5f0653c21f3ad
Ronald Ng <ronald@rescuethecodes.com>
2014-11-03 12:08:45 +0800

    + swagger-js wrapper in JsonApi

--- app/scripts/services/jsonapi.js
+++ app/scripts/services/jsonapi.js
@@ -8,89 +8,93 @@
  * Service in the crudKitApp.
  */
 angular.module('crudKit')
-  .service('JsonApi', ['crudKitConfig','$q', '$rootScope', function JsonApi(config, $q, $rootScope) {
+  .service('JsonApi', ['crudKitConfig','$q', '$rootScope', '$timeout', function JsonApi(config, $q, $rootScope, $timeout) {
     var
       api = this,
       broadcastApiEvent = function(resourceName, eventName, data){
         $rootScope.$broadcast(resourceName.name + "_" + eventName, data);
       };
 
-    api.ready = $q.defer();
+    api._d = $q.defer();
+    api._init = function(){
+      switch(config.schemaType){
+        case "json-schema":
+          throw new Error("API service for json-schema not implemented.");
+          break;
 
-    switch(config.schemaType){
-      case "json-schema":
-        throw new Error("API service for json-schema not implemented.");
-        break;
+      /** Create local Models based on remote swagger spec (using swagger-js lib) **/
+        case "swagger":
+          var
+            defaultApiMethodNames = ['count', 'create', 'deleteById', 'exists', 'find', 'findById', 'findOne', 'updateAll', 'upsert'],
+            makeDefaultApiMethods = function(apiName){
+              var
+                swaggerResource = api.swagger.apis[apiName],
+                defaultApiMethods = {};
 
-    /** Create local Models based on remote swagger spec (using swagger-js lib) **/
-      case "swagger":
-        var
-          defaultApiMethodNames = ['count', 'create', 'deleteById', 'exists', 'find', 'findById', 'findOne', 'updateAll', 'upsert'],
-          makeDefaultApiMethods = function(apiName){
-            var
-              swaggerResource = api.swagger.apis[apiName],
-              defaultApiMethods = {};
+              for (var k in swaggerResource){
+                var element = swaggerResource[k];
+                if (typeof element === 'function'){
+                  var
+                    kExploded = k.split("_"),
+                    methodName = kExploded.pop();
+                  if (defaultApiMethodNames.indexOf(methodName)){
+                    // Create the actual method, e.g. count()
+                    (function(thisMethodName, thisApiName){
+                      defaultApiMethods[thisMethodName] = function(args, successCb, errorCb){
+                        successCb = successCb || function(res){};
+                        errorCb = errorCb || function(err){};
 
-            for (var k in swaggerResource){
-              var element = swaggerResource[k];
-              if (typeof element === 'function'){
-                var
-                  kExploded = k.split("_"),
-                  methodName = kExploded.pop();
-                if (defaultApiMethodNames.indexOf(methodName)){
-                  // Create the actual method, e.g. count()
-                  (function(thisMethodName, thisApiName){
-                    defaultApiMethods[thisMethodName] = function(args, successCb, errorCb){
-                      successCb = successCb || function(res){};
-                      errorCb = errorCb || function(err){};
+                        // Wrap swagger api call with promise
+                        var apiMethodPromise = $q.defer();
+                        this(args, function(res){
+                          // Success CB
+                          apiMethodPromise.resolve(res);
+                          successCb(res);
+                          broadcastApiEvent(thisApiName, "after_" + thisMethodName, res);
+                        }, function(err){
+                          // Error CB
+                          apiMethodPromise.reject(err);
+                          broadcastApiEvent(thisApiName, thisMethodName + "_error", res);
+                          errorCb(err);
+                        });
 
-                      // Wrap swagger api call with promise
-                      var apiMethodPromise = $q.defer();
-                      this(args, function(res){
-                        // Success CB
-                        apiMethodPromise.resolve(res);
-                        successCb(res);
-                        broadcastApiEvent(thisApiName, "after_" + thisMethodName, res);
-                      }, function(err){
-                        // Error CB
-                        apiMethodPromise.reject(err);
-                        broadcastApiEvent(thisApiName, thisMethodName + "_error", res);
-                        errorCb(err);
-                      });
-
-                      return apiMethodPromise.promise;
-                    }.bind(element);
-                  })(methodName, apiName);
+                        return apiMethodPromise.promise;
+                      }.bind(element);
+                    })(methodName, apiName);
+                  }
                 }
+              } // end for each potential api method
+
+              return defaultApiMethods;
+            };
+
+          api.swaggerUrl = config.schemaUrl;
+
+          api.swagger = new SwaggerApi({
+            url: api.swaggerUrl,
+            success: function(){
+              if(api.swagger.ready === true){
+                api.swagger.apisArray.forEach(function createApiModel(swaggerApi){
+                  var apiName = swaggerApi.name;
+                  api[apiName] = makeDefaultApiMethods(apiName);
+                });
+
+                $rootScope.$apply(function(){
+                  console.info("SwaggerApi ready.");
+                  api._d.resolve(api);
+                }); // $rootScope.$apply()
               }
-            } // end for each potential api method
-
-            return defaultApiMethods;
-          };
-
-        api.swaggerUrl = config.schemaUrl;
-        api.swagger = new SwaggerApi({
-          url: api.swaggerUrl,
-          success: function(){
-            if(api.swagger.ready === true){
-              api.swagger.apisArray.forEach(function createApiModel(swaggerApi){
-                var apiName = swaggerApi.name;
-                api[apiName] = makeDefaultApiMethods(apiName);
-              });
-
-              console.info("SwaggerApi ready.");
-              api.ready.resolve(api);
-              api._isReady = true;
-            } else {
-              api.ready.reject("Failed to initiate SwaggerApi");
             }
-          }
-        }); // new SwaggerApi
-        break;
+          }); // new SwaggerApi
+          break;
 
-      default:
-        throw new Error("API service not implemented for this schema type");
-    }
+        default:
+          throw new Error("API service not implemented for this schema type");
+      }
+    }; // api._init()
+
+    api.ready = api._d.promise;
+    api._init();
 
     return api;
   }]);
